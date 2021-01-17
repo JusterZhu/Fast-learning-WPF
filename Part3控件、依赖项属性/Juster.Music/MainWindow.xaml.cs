145999814d60c34f4bb24c181f2aa9280335f8dc
朱震 <zhuzhen723723@outlook.com>
2021-01-17 21:06:28 +0800

    Part3控件、依赖项属性

---
+++ Part3控件、依赖项属性/Juster.Music/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Juster.Music
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            string a = TxtA.Text;
+            string p = TxtP.Text;
+
+            if (a == "zhangsan" && p == "123456")
+            {
+                MessageBox.Show("登录成功！");
+            }
+            else
+            {
+                MessageBox.Show("登录失败！");
+            }
+        }
+    }
+}
