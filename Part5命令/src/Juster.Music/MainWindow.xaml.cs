8f911b9ab89c723954b4a0f26d17c53e03e4ae85
朱震 <zhuzhen723723@outlook.com>
2021-01-24 23:59:03 +0800

    Part5命令

---
+++ Part5命令/src/Juster.Music/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Juster.Music
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new TempModel();
+        }
+
+        //public MainWindow()
+        //{
+        //    InitializeComponent();
+        //    //把命令赋值给命令源（发送者），并指定快捷键
+        //    this.button1.Command = clearCmd;
+
+        //    //指令命令目标
+        //    this.button1.CommandTarget = this.textBoxA;
+
+        //    //创建命令关联
+        //    CommandBinding cb = new CommandBinding();
+        //    cb.Command = this.clearCmd;    //只关注与clearCmd命令相关的事件
+        //    cb.CanExecute += cb_CanExecute;
+        //    cb.Executed += cb_Executed;
+
+        //    //把命令安置在外围控件上
+        //    this.stackPanel.CommandBindings.Add(cb);
+        //}
+
+        ///// <summary>
+        ///// 当命令送达目标后，此方法被调用
+        ///// </summary>
+        ///// <param name="sender"></param>
+        ///// <param name="e"></param>
+        //void cb_Executed(object sender, ExecutedRoutedEventArgs e)
+        //{
+        //}
+
+        ///// <summary>
+        ///// 探测命令是否可执行时，此方法被调用
+        ///// </summary>
+        ///// <param name="sender"></param>
+        ///// <param name="e"></param>
+        //void cb_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        //{
+        //}
+    }
+}
---
+++ Part5命令/src/Juster.Music/TempModel.cs
@@ -0,0 +1,77 @@
+﻿using Juster.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Juster.Music
+{
+    public class TempModel 
+    {
+        private string _account;
+
+        public string Account
+        {
+            get { return _account; }
+            set 
+            { 
+                _account = value;
+            }
+        }
+
+        private string _passwrold;
+
+        public string PassWrold
+        {
+            get { return _passwrold; }
+            set 
+            { 
+                _passwrold = value;
+            }
+        }
+
+        private List<string> _loginTypes;
+
+        public List<string> LoginTypes
+        {
+            get { return _loginTypes; }
+            set { _loginTypes = value; }
+        }
+
+        public ICommand LoingCommand { get; set; }
+
+        public ICommand CancelRemberCommand { get; set; }
+
+        public TempModel() 
+        {
+            LoingCommand = new RelayCommand(LoginAction);
+            //LoingCommand.Execute(null);
+            CancelRemberCommand = new RelayCommand<bool>(CancelAction);
+            Account = "root";
+            LoginTypes = new List<string>();
+            LoginTypes.Add("手机号登录");
+            LoginTypes.Add("账号密码登录");
+            LoginTypes.Add("二维码登录");
+        }
+
+        private void CancelAction(bool obj)
+        {
+            MessageBox.Show($"check status { obj }");
+        }
+
+        private void LoginAction()
+        {
+            if (Account == "root" && PassWrold == "123")
+            {
+                MessageBox.Show("登录成功！");
+            }
+            else
+            {
+                MessageBox.Show("登录失败！");
+            }
+        }
+    }
+}
