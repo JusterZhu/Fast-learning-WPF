ffd17e20428669f0c35f7d0503c02ebecbe0bfe4
朱震 <zhuzhen723723@outlook.com>
2021-01-27 23:54:28 +0800

    Part6MVVM

---
+++ Part6MVVM/src/Juster.Common/RelayCommand.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Input;
+
+namespace Juster.Common
+{
+    public class RelayCommand : ICommand
+    {
+        #region 字段
+
+        readonly Func<Boolean> _canExecute;
+
+        readonly Action _execute;
+
+        #endregion
+
+        #region 构造函数
+
+        public RelayCommand(Action execute)
+            : this(execute, null)
+        {
+
+        }
+
+        public RelayCommand(Action execute, Func<Boolean> canExecute)
+        {
+            if (execute == null)
+                throw new ArgumentNullException("execute");
+
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        #endregion
+
+        #region ICommand的成员
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                if (_canExecute != null)
+                CommandManager.RequerySuggested += value;
+            }
+
+            remove
+            {
+                if (_canExecute != null)
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        [DebuggerStepThrough]
+
+        public Boolean CanExecute(Object parameter)
+        {
+            return _canExecute == null ? true : _canExecute();
+        }
+
+        public void Execute(Object parameter)
+        {
+            _execute();
+        }
+
+        #endregion
+    }
+
+    /// <summary>
+    /// A command whose sole purpose is to 
+    /// relay its functionality to other
+    /// objects by invoking delegates. The
+    /// default return value for the CanExecute
+    /// method is 'true'.
+    /// </summary>
+    public class RelayCommand<T> : ICommand
+    {
+        #region Constructors
+
+        public RelayCommand(Action<T> execute)
+            : this(execute, null)
+        {
+        }
+
+        /// <summary>
+        /// Creates a new command.
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        /// <param name="canExecute">The execution status logic.</param>
+        public RelayCommand(Action<T> execute, Predicate<T> canExecute)
+        {
+            if (execute == null)
+                throw new ArgumentNullException("execute");
+
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        #endregion // Constructors
+
+        #region ICommand Members
+
+        [DebuggerStepThrough]
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute((T)parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                if (_canExecute != null)
+                    CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                if (_canExecute != null)
+                    CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute((T)parameter);
+        }
+
+        #endregion // ICommand Members
+
+        #region Fields
+
+        readonly Action<T> _execute = null;
+        readonly Predicate<T> _canExecute = null;
+
+        #endregion // Fields
+    }
+}
---
+++ Part6MVVM/src/Juster.Music/ViewModels/MainViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Juster.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Juster.Music.ViewModels
+{
+    public class MainViewModel
+    {
+      
+    }
+}
